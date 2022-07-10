8d22b3f8b73fa26ff0860ff2a129ddeaaf9dbf7b
Jakub M <fotograflubin@gmail.com>
2022-07-10 15:49:06 +0200

    Done. With optional

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,34 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String valueOfFur;
+        String valOfPaws;
+
+        if (hasFur) {
+            valueOfFur = "a";
+        } else {
+            valueOfFur = "no";
+        }
+
+        if (numberOfPaws > 1) {
+            valOfPaws = "paws";
+        } else
+            valOfPaws = "paw";
+
+
+        return "This animal is mostly " + color + "." + " It has " +
+                numberOfPaws + " "+ valOfPaws + " and " + valueOfFur + " fur.";
+    }
 
 }
